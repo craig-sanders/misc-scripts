49ccf1e559c1c4e1cca7f5eb9107d61b934363bf
Craig Sanders <cas@taz.net.au>
2026-02-21 14:44:54 +1100

    grub-list-kernels.pl: various relatively minor changes

--- grub-list-kernels.pl
+++ grub-list-kernels.pl
@@ -1,42 +1,49 @@
-#! /usr/bin/perl 
+#!/usr/bin/perl
 
-# parse and list the boot entries in the grub boot menu (grub.cfg)
-#
-# $Id: grub-list-kernels.pl,v 1.9 2016/06/06 23:22:48 root Exp $
+# Parse and list the boot entries in the grub boot menu (grub.cfg)
 
-# Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012, 2013 Craig Sanders <cas@taz.net.au>
+# Copyright (C) 2007-2026 Craig Sanders <cas@taz.net.au>
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2, or (at your option)
-# any later version.
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any later
+# version.
 
 # Partial Revision History:
+# 2026-02-21 - Use variables for file handles rather than barewords.
+#            - Use 3-argument open()
+#            - Tweaked printf statements and other output
+#            - -v output uses %55s for the id now, but it really only looks
+#              good on >80 cols because grub's /etc/grub.d/ scripts tend to
+#              make really long ids.
+#            - Added a usage() function for --help
+#
 # 2015-07-24 - added --verbose option for use with grub-set-default
 #              fixed display of default boot item when it is "submenuid>menuentry_id_option"
 # 2013-06-20 - better handling of non-numeric default saved entries, code still uglyish but better commented.
 # 2013-05-13 - first stab at support for submenus...works but uglified the code
 # 2010-11-03 - rewrote for grub2 (grub.cfg rather than menu.lst)
+#
+# TODO: rewrite so that it's not such an ugly hack
 
 use strict;
 
 use Getopt::Long;
 
 our $verbose = 0;   # verbose format displays menuidoption for use with grub-set-default
-my $debug = 0 ;
 my $help = 0;
 
-GetOptions('help|?|h' => \$help, 
-           'verbose|v' => \$verbose, 
-           'debug|d' => \$debug, 
+GetOptions('help|?|h' => \$help,
+           'verbose|v' => \$verbose,
            ) ;
 
+usage() if ($help);
 
 my $cfg = shift || '/boot/grub/grub.cfg';
 
 my $grubenv='/boot/grub/grubenv';
 
-# hash of kernels (menu entry index -> menu entry description) 
+# hash of kernels (menu entry index -> menu entry description)
 # e.g. '4' => 'Debian GNU/Linux, with Linux 3.8-1-amd64'
 my %K = ();
 
@@ -46,7 +53,7 @@ my %M = ();
 
 # read in the menu entries.
 
-open(MENU,"<$cfg") || die "couldn't open $cfg for read: $!\n" ;
+open(my $menu_fh, "<", "$cfg") || die "couldn't open $cfg for read: $!\n" ;
 
 my $insubmenu = 0;
 my $menuitem=0;
@@ -56,7 +63,7 @@ my $menuindex='';
 my $kernel='';
 my $menuidoption='';
 
-while (<MENU>) {
+while (<$menu_fh>) {
   #next unless /^\s*menuentry\s/io;
   chomp;
   if ($insubmenu ne 0) {
@@ -77,16 +84,13 @@ while (<MENU>) {
     $menuindex = $menuitem++;
   }
 
-  # sometimes the default entry saved in grubenv is the menu_id_option
+  # Sometimes the default entry saved in grubenv is the menu_id_option
   # rather than numeric.  The %M hash links menu_id_option to menuindex
   # numbers.
   # e.g. 'gnulinux-3.8-1-amd64-advanced-6bb6d228-0581-49ae-9d49-dd148c273ecc' => 4
   $menuidoption='';
-  my $line = $_;
-  if ($line =~ m/menuentry_id_option/) { 
-      $line =~ s/.*menuentry_id_option '//;
-      $line =~ s/'.*//;
-      $menuidoption=$line;
+  if (m/(?:\$menuentry_id_option|--id)\s*['"]?(.*?)['"]?\s/) {
+      $menuidoption=$1;
       $M{$menuidoption} = $menuindex;
   };
 
@@ -97,23 +101,23 @@ while (<MENU>) {
   $K{$menuindex} = $kernel;
   #printf "%4s\t%s\t%s\n", $menuindex, $_, $menuidoption;
   if ($verbose eq 1) {
-    printf "%4s\t%s\t%s\t\n", $menuindex, $menuidoption, $_;
+    printf "%2s %-55s\t%s\n", $menuindex, $menuidoption, $_;
   } else {
-    printf "%4s\t%s\t\n", $menuindex, $_;
+    printf "%2s %s\n", $menuindex, $_;
   } ;
 } ;
 
+close($menu_fh);
 
-
-# now get the default kernel and boot-once kernel if any
+# Now get the default kernel and boot-once kernel if any
 
 my $saved_entry='';
 my $prev_saved_entry='';
 my $defk = 0;
 my $once = 0;
 
-open(GRUBENV, "<$grubenv") || die "couldn't open $grubenv for read: $!\n" ;
-while (<GRUBENV>) {
+open(my $grubenv_fh, "<", $grubenv) || die "couldn't open $grubenv for read: $!\n" ;
+while (<$grubenv_fh>) {
   chomp;
   next unless /saved_entry/;
   if (/^saved_entry/) {
@@ -122,7 +126,7 @@ while (<GRUBENV>) {
     (undef,$prev_saved_entry) = split /=/;
   }
 }
-close(GRUBENV);
+close($grubenv_fh);
 
 $saved_entry = 0 if ($saved_entry eq '');
 
@@ -146,7 +150,7 @@ if ($once !~ /^[0-9]+$/) {
   $once = $M{$once};
 }
 
-print "\n  Default: $defk";
+print "\nDefault: $defk";
 print "  \"", $K{$defk}, "\"\n";
 
 if ($prev_saved_entry ne '') {
@@ -154,3 +158,20 @@ if ($prev_saved_entry ne '') {
   print "  \"", $K{$once}, "\"\n";
 }
 
+
+sub usage {
+  use File::Basename;
+  my $script = basename $0;
+
+  printf <<__EOF__;
+$script [--help] [--verbose] [grub.cfg]
+
+   --help, -?, -h      Show this help message
+   --verbose, -v       Show menu entry IDs as well as index numbers
+
+The optional grub.cfg argument defaults to /boot/grub/grub.cfg
+
+__EOF__
+
+  exit 0;
+}
